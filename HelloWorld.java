6a19767751294576ab783c67322c748617c02998
Greg Reid <41651311+stfxcomsci@users.noreply.github.com>
2025-04-15 10:16:10 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+//import java.util.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Creat a scanner to read
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your name: ");
+        // String name = sc.next();
+        // System.out.println("Hello "+name);
+        // sc.close();   
+        
+        int num = 4;
+        System.out.println(num);
+        System.out.println(num++);
+        System.out.println(++num);
+        System.out.println(num);
+
+    }
+}
